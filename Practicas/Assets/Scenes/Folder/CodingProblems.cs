84d4a922019f98dc4551324fb67f6d62e316a4cb
RicardoBanuelos <ricardo.banuelos@uabc.edu.mx>
2020-09-26 13:55:16 -0700

    ProblemaPrac1

--- Practicas/Assets/Scenes/Folder/CodingProblems.cs
+++ Practicas/Assets/Scenes/Folder/CodingProblems.cs
@@ -7,12 +7,25 @@ public class CodingProblems : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        int[] nums = {1,2,3,4};
-        smallerNumberCounter(nums);
+        int[] input = {1,2,3,4};
+        int[] output = new int[4];
+        Debug.Log("Solving problem...");
+        output = smallerNumberCounter(input);
     }
 
     public int[] smallerNumberCounter(int[] nums){
-        Debug.Log("Solving problem...");
-        return nums;
+        int[] output = new int[nums.Length];
+        int counter;
+
+        for(int i = 0; i < nums.Length; i++){
+            counter = 0;
+            for(int j = 0; j < nums.Length; j++){
+                if(nums[j] < nums[i])
+                    counter++;
+            }
+            output[i] = counter;
+        }
+
+        return output;
     }
 }
