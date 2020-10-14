fbc3df7e4b0ee2dce3423c0a1d3e6daa038e2f29
RicardoBanuelos <ricardo.banuelos@uabc.edu.mx>
2020-10-13 19:03:29 -0700

    Moneda Agarrable

---
+++ Practicas/Assets/Scripts/Interactable.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public bool inZone = false;
+
+    public virtual void Interact()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(!other.CompareTag("Player"))
+        {
+            return;
+        }
+        Debug.Log("ok");
+        inZone = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(!other.CompareTag("Player"))
+        {
+            return;
+        }
+        inZone = false;
+    }
+}
---
+++ Practicas/Assets/Scripts/PickUpCoin.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpCoin : Interactable
+{
+    public override void Interact()
+    {
+        Debug.Log("Coin collected.");
+        Object.Destroy(this.gameObject);
+    }
+
+    void Update()
+    {
+        if(inZone && Input.GetKeyDown(KeyCode.C))
+            Interact();
+    }
+}
