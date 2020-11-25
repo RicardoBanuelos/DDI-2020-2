8ae70616f9771fd46acf224ccb6e9ce881def6e2
RicardoBanuelos <ricardo.banuelos@uabc.edu.mx>
2020-11-25 14:00:45 -0800

    Practica6

---
+++ GoogleVR/Assets/Scripts/Locomotion.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Locomotion : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 heightOffset;
+    public void TeleportPlayer(Vector3 newPos)
+    {
+        player.position = newPos + heightOffset;
+    }
+
+}
