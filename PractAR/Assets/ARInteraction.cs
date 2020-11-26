9eecbfc98848f3837ec763a6d13a67facf0a833c
RicardoBanuelos <ricardo.banuelos@uabc.edu.mx>
2020-11-25 16:17:44 -0800

    Practica7

---
+++ PractAR/Assets/ARInteraction.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ARInteraction : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        Debug.Log("Rumm Rummm");
+        Destroy(this.gameObject);
+    }
+    
+}
